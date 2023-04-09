18f6d7f38dc491540a3d35988cef1406b62d52aa
Maira Javed <mairajaved010@gmail.com>
2023-04-09 21:05:36 +0500

    done

--- scripts/practice.js
+++ scripts/practice.js
@@ -50,3 +50,49 @@ var anotherNum = 1;
 var popularNumber = num + anotherNum;
 var whatsLeftOver = 10 % 3;
 var whatsLeftOver = 9 % 3;
+//day 02
+num++;
+num = num + 1;
+num--;
+
+ var num = 1;
+ var newNum = num++;
+
+ var num = 1;
+  var newNum = ++num;
+  var num = 1;
+   var newNum = --num;
+   var totalCost = 1 + 3 * 4;
+   var totalCost = 1 + (3 * 4);
+var totalCost = (1 + 3) * 4;
+var resultOfComputation = (2 * 4) * 4 + 2;
+resultOfComputation = ((2 * 4) * 4) + 2;
+resultOfComputation = (2 * 4) * (4 + 2);
+//day 03
+alert("Thanks for your input!");
+ var message = "Thanks for your input!";
+ alert(message);
+
+alert("Thanks, " + userName + "!");
+
+
+ var message = "Thanks, ";
+ var banger = "!";
+ alert(message + userName + banger);
+ alert("Thanks, " + "Susan" + "!");
+
+ var message = "Thanks, ";
+ var userName = "Susan";
+ var banger = "!";
+ var customMess = message + userName + banger;
+ alert(customMess);
+alert("2" + "2");
+
+alert("2 plus 2 equals " + 2 + 2);
+var spec = prompt("Your species?", "human");
+var question = "Your species?";
+ var defaultAnswer = "human";
+ var spec = prompt(question, defaultAnswer);
+ var numberOfCats = prompt("How many cats?");
+  var tooManyCats = numberOfCats + 1;
+ 
